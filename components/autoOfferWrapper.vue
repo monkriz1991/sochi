0b37e4e8bc2deec28312989b94ee1da53345a403
Alex Chow <cat@9h0st.ru>
2020-03-31 15:14:16 +0300

    l3 fix

--- components/autoOfferWrapper.vue
+++ components/autoOfferWrapper.vue
@@ -4,30 +4,37 @@
       div(v-if="loading").p-5
         loader
       div(v-else)
-        div(v-if="timeFalse")
+        div(v-if="coronaFalse")
           div.offer.m-5.p-5
             h4.text-center.text-uppercase
-              |{{$t('s1')}}
+              |{{$t('s0')}}
               br
               a.mgo-number(:href="`tel:${$assets.cleanPhone(settings.main_phone)}`")="{{settings.main_phone}}"
         div(v-else)
-          div(v-if="searchForm.isDriver")
+          div(v-if="timeFalse")
             div.offer.m-5.p-5
               h4.text-center.text-uppercase
-                |{{$t('s2')}}
+                |{{$t('s1')}}
                 br
                 a.mgo-number(:href="`tel:${$assets.cleanPhone(settings.main_phone)}`")="{{settings.main_phone}}"
           div(v-else)
-            div(v-if="filtered.length > 0")
-              b-row.d-flex.align-items-center.justify-content-center
-                b-col(sm="12" md="6" lg="4" v-for="(item, idx) in filtered" :key="idx")
-                  auto-offer-card(:item="item").my-3
-            div(v-else)
+            div(v-if="searchForm.isDriver")
               div.offer.m-5.p-5
                 h4.text-center.text-uppercase
-                  |{{$t('s3')}}
+                  |{{$t('s2')}}
                   br
-                  a(href="tel:+74951084848")="+7 (495) 108-48-48"
+                  a.mgo-number(:href="`tel:${$assets.cleanPhone(settings.main_phone)}`")="{{settings.main_phone}}"
+            div(v-else)
+              div(v-if="filtered.length > 0")
+                b-row.d-flex.align-items-center.justify-content-center
+                  b-col(sm="12" md="6" lg="4" v-for="(item, idx) in filtered" :key="idx")
+                    auto-offer-card(:item="item").my-3
+              div(v-else)
+                div.offer.m-5.p-5
+                  h4.text-center.text-uppercase
+                    |{{$t('s3')}}
+                    br
+                    a(href="tel:+74951084848")="+7 (495) 108-48-48"
     hr(v-if="!loading").dashed
 </template>
 
@@ -38,7 +45,7 @@
   export default {
     name: "autoOfferWrapper",
     components: {
-     autoOfferCard,
+      autoOfferCard,
       loader
     },
     data(){
@@ -46,6 +53,7 @@
         cars: [],
         loading: true,
         timeFalse: false,
+        coronaFalse: false,
       }
     },
     computed:{
@@ -81,22 +89,27 @@
         let df_sec = (new Date(this.searchForm.df)).getTime();
         let now_sec = (new Date()).getTime();
         let od = 86400000;
-        if((df_sec - now_sec) > od){
-          this.loading = true;
-          this.$axios.post('cars', this.$assets.prepareForm(this.searchForm))
-            .then(result => {
-              if (yaCounter33072038){
-                yaCounter33072038.reachGoal('Findаcar');
-              }
-              if (result.data.status === 'success'){
-                this.cars = result.data.data;
-                this.loading = false;
-                this.timeFalse = false;
-              }
-            })
-            .catch(err => console.error(err))
+        if (Date.parse(this.searchForm.df) > Date.parse('2020-03-05')){
+          if((df_sec - now_sec) > od){
+            this.loading = true;
+            this.$axios.post('cars', this.$assets.prepareForm(this.searchForm))
+              .then(result => {
+                if (yaCounter33072038){
+                  yaCounter33072038.reachGoal('Findаcar');
+                }
+                if (result.data.status === 'success'){
+                  this.cars = result.data.data;
+                  this.loading = false;
+                  this.timeFalse = false;
+                }
+              })
+              .catch(err => console.error(err))
+          }else{
+            this.timeFalse = true;
+            this.loading = false;
+          }
         }else{
-          this.timeFalse = true;
+          this.coronaFalse = true;
           this.loading = false;
         }
       }
