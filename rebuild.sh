169aa858fd48df443da1fd50e1fca7e24942eec0
Alex Chow <cat@9h0st.ru>
2020-04-05 21:40:14 +0300

    translation

--- rebuild.sh
+++ rebuild.sh
@@ -3,11 +3,13 @@ echo "входим в папку"
 cd /home/cat/web-server/sochirentacar.ru/public_html
 rm -f cars.json
 rm -f news.json
+rm -f static/sitemap.xml
 echo "качаем репо"
 git pull origin master
 echo "качаем файлы"
 wget -O cars.json https://booking.autopilot.rent/api/sun/cars/getSeo/sochi.json
 wget -O news.json https://booking.autopilot.rent/api/sun/newsListSeo/sochi.json
+wget -O static/sitemap.xml https://booking.autopilot.rent/sun/sun-sites-sitemap/sochi
 echo "билдим"
 yarn
 yarn build
