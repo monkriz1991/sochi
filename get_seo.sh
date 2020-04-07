c8c086edb916973167c88384145360a0aa447ffa
Alex Chow <cat@9h0st.ru>
2020-04-07 23:46:33 +0300

    translation

--- get_seo.sh
+++ get_seo.sh
@@ -4,5 +4,7 @@ rm -f cars.json
 wget -O cars.json https://booking.autopilot.rent/api/sun/cars/getSeo/sochi.json
 rm -f news.json
 wget -O news.json https://booking.autopilot.rent/api/sun/newsListSeo/sochi.json
+rm -f static/sitemap.xml
+wget -O static/sitemap.xml https://booking.autopilot.rent/sun/sun-sites-sitemap/sochi
 yarn build
 pm2 restart 7
