a88803929609d7a1915a6c2b15677a88dc38be8f
Alex Chow <cat@9h0st.ru>
2020-05-04 10:04:06 +0300

    hotfix

--- classes/assets.js
+++ classes/assets.js
@@ -323,7 +323,7 @@ class Assets {
 
   checkAbhazAvailable = (name, df) =>{
     if (name === "Выезд в Абхазию"){
-      if (Date.parse(df) > Date.parse('2020-05-01')){
+      if (Date.parse(df) > Date.parse('2021-05-01')){
         return false
       }else{
         return true
