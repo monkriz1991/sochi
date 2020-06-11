9a017312c36d6a37dc20c172f684009549139a70
Alexandr Chow <cat@9h0st.ru>
2020-06-11 11:43:05 +0300

    pp

--- components/CWoDComponentFull.vue
+++ components/CWoDComponentFull.vue
@@ -13,8 +13,6 @@
               b-col(sm="12" md="12" lg="6")
                 div.el-wrapper
                   div.price-badge(v-html="`${$t('cwd27')} ${$assets.toMoney(i.car_data.stoimost - ((i.car_data.stoimost/ 100) * i.car_data.skidka_2))}₽/${$t('cwod7')}`")
-                  span(v-if="i.self_data.is_blocked").badge-status.no="{{$t('df108')}}"
-                  span(v-else).badge-status.yes="{{$t('df109')}}"
                   div.main-img(v-bind:style="{backgroundImage:`url(https://booking.autopilot.rent/${i.self_data.photos[0]})`}")
                   div.flox-img(v-bind:style="{backgroundImage:`url(https://booking.autopilot.rent/${i.self_data.photos[1]})`}")
                   div.flox-img(v-bind:style="{backgroundImage:`url(https://booking.autopilot.rent/${i.self_data.photos[2]})`}")
