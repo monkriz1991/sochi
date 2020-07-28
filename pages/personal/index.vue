9dbaf14aabcd1e8c4fefe4cfc4b4cdfe64df0b8e
Alexandr Chow <cat@9h0st.ru>
2020-07-28 17:09:58 +0300

    hotfix

--- pages/personal/index.vue
+++ pages/personal/index.vue
@@ -21,7 +21,24 @@
                   nuxt-link(:to="{name: $assets.prefix('personal-registation', $i18n.locale)}")="{{ $t('lk12') }}"
                 b-col(sm="12" md="6" lg="6").text-center
                   nuxt-link(:to="{name: $assets.prefix('personal-renew_password', $i18n.locale)}")="{{ $t('lk13') }}"
-          b-col(sm="12" md="6" lg="6")
+          b-col(sm="12" md="6" lg="6").my-0.my-sm-3.my-md-0.my-lg-0
+            h5
+              |Добро пожаловать в
+              b.mx-2.text-nowrap
+                span.b-text="AUTOPILOT"
+                span="."
+                span.r-text="LOYALTY"
+
+            p.my-2
+              |Зарегистрируйтесь в программе
+              b.mx-2.text-nowrap
+                span.b-text="AUTOPILOT"
+                span="."
+                span.r-text="LOYALTY"
+              |до того, как решите забронировать автомобиль.
+            p.my-2="Вам будет присвоен персональный номер участника, в который будут внесены ваши личные данные, что поможет сделать процесс бронирования еще проще и быстрее."
+            p.my-2="Наш сервис быстрого обслуживания и заранее подготовленные документы позволят оказаться за рулем автомобиля в считанные минуты."
+            p.my-2="Автомобиль будет ждать Вас в отведенном месте на парковке."
 </template>
 
 <script>
@@ -52,5 +69,8 @@
 </script>
 
 <style lang="sass" scoped>
-
+  .r-text
+    color: #d52b1e
+  .b-text
+    color: #0039a6
 </style>
