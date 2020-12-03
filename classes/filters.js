ad1acb3d23b13c14ce390ed62602306a945adfd4
Alexandr Chow <cat@9h0st.ru>
2020-12-03 15:52:23 +0300

    filter.color fix

--- classes/filters.js
+++ classes/filters.js
@@ -21,9 +21,9 @@ class Filters{
     let res = [];
     data.map((el)=>{
       if (!this.checkAvailibility(el.naimenovanie, el.Period)){
-        if (elements.indexOf(`${el.naimenovanie}`) + 1){
+        if (elements.indexOf(`${el.naimenovanie}_${el.cvet}`) + 1){
         }else{
-          elements.push(`${el.naimenovanie}`)
+          elements.push(`${el.naimenovanie}_${el.cvet}`)
           res.push(el)
         }
       }
