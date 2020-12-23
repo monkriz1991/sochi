531ce48c3753352355e50da37f9dca9f70c6215b
Alexandr Chow <cat@9h0st.ru>
2020-12-23 13:23:43 +0300

    faq fix

--- pages/about/faq/index.vue
+++ pages/about/faq/index.vue
@@ -61,119 +61,142 @@
         faq: {
           ru: [
             {
-              q: "Что будет с залогом когда закончится срок аренды?",
-              a: "Сумма залога в обязательном порядке будет возвращена Вам по окончании срока аренды автомобиля, сразу после подписания Акта приема-передачи."
+              'q': 'Какие документы необходимо предоставить для аренды автомобиля?',
+              "a": 'паспорт РФ (главная страница и прописка)/заграничный паспорт (только для иностранных граждан), водительское удостоверение (с двух сторон).'
             },
             {
-              q: "Услуга \"Отмена франшизы\" - что это?",
-              a: "Услуга \"Отмена франшизы\" за дополнительную плату освобождает отменяет для Вас франшизу по полису КАСКО, что позволит Вам не беспокоиться о происшествиях на дорогах Краснодарского края.",
+              'q': 'Когда будет готов Договор и другие необходимые документы?',
+              "a": 'Ваш Договор аренды и другие сопутствующие документы будут готовы к моменту получения автомобиля.'
             },
             {
-              q: "Обязательно ли оплачивать аренду заранее?",
-              a: "Оплатить аренду и внести залог Вы можете непосредственно при получении автомобиля. В случае поступления оплаты для Вас будет забронирован конкретный автомобиль, персональные данные которого (цвет, комплектация, год выпуска, регистрационный знак) будут указаны в Договоре аренды и Ваучере на получение автомобиля, которые мы до начала срока аренды направим Вам на электронную почту.",
+              'q': 'Автомобили застрахованы?',
+              "a": 'Все автомобили нашей компании застрахованы по КАСКО и ОСАГО. Согласно Договору аренды Ваша ответственность ограничена франшизой по полису КАСКО. Для снятия ограничений Вы можете воспользоваться услугой «Отмена франшизы» или выбрать услугу «Расширенное страховое покрытие».'
             },
             {
-
-              q: "Как внести предоплату?",
-              a: "Вы можете внести предоплату как с помощью онлайн сервиса на сайте нашей компании, так и наличными или с помощью банковской карты любого банка в нашем офисе.",
+              'q': 'Что будет с залогом, когда закончится срок аренды?',
+              "a": 'Сумма залога будет возвращена вам в обязательном порядке в течение 30 календарных дней после окончания срока аренды автомобиля, при соблюдении всех условий Договора.'
             },
             {
-              q: "Может ли к управлению автомобилем быть допущен еще один водитель?",
-              a: "К управлению автомобилем могут быть допущены другие водители при условии соответствия требованиям, предъявляемым к Арендатору, а также при наличии аналогичного набора документов.",
+              'q': 'Обязательно ли вносить предоплату?',
+              "a": 'Предоплата в размере 20% от стоимости аренды, является обязательным условием для бронирования автомобиля. После внесения предоплаты, для Вас будет забронирован конкретный автомобиль, персональные данные которого (цвет, комплектация, год выпуска, регистрационный знак) будут указаны в Договоре аренды и Ваучере на получение автомобиля.'
             },
             {
-              q: "Что делать, если автомобиль выйдет из строя не по моей вине?",
-              a: "В случае поломки мы оперативно и бесплатно заменим автомобиль на аналогичный.",
+              'q': 'Как внести предоплату?',
+              "a": 'Вы можете внести предоплату с помощью онлайн сервиса на сайте нашей компании.'
             },
             {
-              q: "Какой порядок получения автомобиля в Аэропорту Сочи?",
-              a: "По прилету в Аэропорт города Сочи Вам необходимо подойти к стойке нашей компании, которая находится в терминале прилета.",
+              'q': 'Можно ли оплатить аренду картой?',
+              "a": 'Вы можете оплатить аренду автомобиля с помощью карты Master Card, Visa или МИР в офисе нашей компании.'
             },
             {
-              q: "Обязательно ли в течение дня оставлять автомобиль на охраняемых парковках или стоянках?",
-              a: "В течение дня мы рекомендуем Вам без крайней необходимости не оставлять автомобиль без присмотра. А в темное время суток желательно хранить автомобиль на охраняемых стоянках (парковках).",
+              'q': 'Как найти Вас в Аэропорту города Сочи?',
+              "a": 'По прилету в Аэропорт города Сочи наш менеджер свяжется с Вами. Передача автомобиля осуществляется на парковке Аэропорта города Cочи.'
             },
             {
-              q: "Что делать если я попал в ДТП?",
-              a: " В случае ДТП Вам необходимо связаться с нашими специалистами по номеру +7 (988) 282-97-97, затем вызвать на место происшествия сотрудников ГАИ и получить следующие документы: копию Протокола, схему ДТП и справку о ДТП.",
+              'q': 'Может ли к управлению автомобилем быть допущен еще один водитель?',
+              "a": 'К управлению автомобилем могут быть допущены другие водители при условии соответствия требованиям, предъявляемым к Арендатору, а также при наличии аналогичного набора документов.'
             },
             {
-              q: "Автомобили застрахованы?",
-              a: "Все автомобили нашей компании застрахованы по КАСКО и ОСАГО. Согласно Договору аренды Ваша ответственность ограничена франшизой по полису КАСКО. Для снятия ограничений Вы можете воспользоваться услугой «Отмена залога и франшизы»."
+              'q': 'Обязательно ли перед возвратом автомобиля его мыть и заправлять?',
+              "a": 'Автомобиль Вы получите чистым, с полным баком бензина. По окончании срока аренды необходимо вернуть его в чистом виде, либо оплатить мойку ТС в размере 1000 рублей. Топливный бак также должен быть полным, либо Вам необходимо будет оплатить недостающее количество топлива по прайсу компании.'
             },
             {
-              q: "Когда будет готов Договор и другие необходимые документы?",
-              a: "Ваш Договор аренды и другие сопутствующие документы будут готовы к моменту получения автомобиля."
+              'q': 'Какое топливо использовать для заправки автомобиля?',
+              "a": 'Практически все наши автомобили заправляются бензином АИ-95, но есть исключения, их следуют заправлять дизельным топливом ДТ. Информация размещена на лючке бензобака, будьте внимательней!'
             },
             {
-              q: "Обязательно ли перед возвратом мыть и заправлять автомобиль?",
-              a: "Автомобиль Вы получите чистым, с полным баком бензина. По окончании срока аренды необходимо вернуть его в чистом виде, либо оплатить мойку ТС в размере 1000 рублей. Топливный бак также должен быть полным, либо Вам необходимо будет оплатить недостающее количество топлива из расчета 60 рублей за литр."
+              'q': 'Услуга "Отмена франшизы" - что это?',
+              "a": 'Услуга "Отмена франшизы" за дополнительную плату отменяет франшизу по полису КАСКО, и вместе с тем обязывает предоставить все справки о повреждении транспортного средства из ГИБДД.'
             },
             {
-              q: "Какое топливо использовать для заправки автомобиля?",
-              a: "Для заправки автомобиля необходимо использовать бензин АИ-95."
+              'q': 'Что делать, если автомобиль выйдет из строя не по моей вине?',
+              "a": 'В случае поломки мы оперативно и бесплатно заменим автомобиль на аналогичный, либо в короткие сроки устраним причину поломки.'
             },
             {
-              q: "Мы планируем путешествие с маленькими детьми, у Вас можно арендовать детские кресла?",
-              a: "Детские кресла и бустеры можно арендовать за дополнительную плату согласно прайсу компании. Необходимо заказать эту услугу заблаговременно."
+              'q': 'Что делать если я попал в ДТП?',
+              "a": 'В случае ДТП Вам необходимо связаться с нашими специалистами по номеру <a href="tel:88004444848">8 (800) 444-48-48</a>, затем вызвать на место происшествия сотрудников ГАИ и получить следующие документы: копию Протокола, схему ДТП и справку о ДТП.'
+            },
+            {
+              'q': 'Необходимо ли вносить Аэропортовый сбор?',
+              "a": 'В нашей компании отсутствуют всевозможные сборы и дополнительные платежи, *Аэропортовый сбор, доплата за зимние шины и набор автомобилиста.*'
+            },
+            {
+              'q': 'Мы планируем путешествие с маленькими детьми, у Вас можно арендовать детские кресла?',
+              "a": 'Детские кресла и бустеры можно арендовать за дополнительную плату согласно прайсу компании. Необходимо заказать эту услугу заблаговременно.'
+            },
+            {
+              'q': 'Каковы ограничения по суточному пробегу?',
+              "a": 'В нашей компании отсутствует ограничение пробега.'
             }
           ],
           en: [
             {
-              q:" what will happen to the Deposit when the lease term ends?",
-              a: "the amount of the Deposit will be returned to You at the end of the rental period, immediately after signing the acceptance Certificate."
+              'q': 'What documents do you need to provide for car rental?',
+              "a": 'Russian passport (home page and registration)/foreign passport (only for foreign citizens), driver`s license (on both sides).'
             },
             {
-              q: "Service \"Cancellation of Deposit and deductible\" - what is it?",
-              a: "the service \"Cancellation of Deposit and deductible\" for an additional fee frees You from making a Deposit and cancels the deductible under the CASCO policy, which will allow You not to worry about accidents on the roads of the Krasnodar territory.",
+              'q': 'When will the Contract and other necessary documents be ready?',
+              "a": 'Your rental Agreement and other related documents will be ready by the time you receive the car.'
             },
             {
-              q: "Do I have to pay rent in advance?",
-              a: "you can Pay the rent and make a Deposit directly upon receipt of the car. If payment is received, a specific car will be reserved for You.its personal data (color, configuration, year of manufacture, registration plate) will be indicated in the rental Agreement and The voucher for receiving the car, which we will send to Your email before the start of the rental period.",
+              'q': 'Are the cars insured?',
+              "a": 'All cars of our company are insured under CASCO and CTP. According to the lease Agreement, your liability is limited to the deductible under the CASCO policy. To remove the restrictions, you can use the "deductible Cancellation" service or choose the "Extended insurance coverage" service.'
             },
             {
-              q: "How do I make a prepayment?",
-              a: "you can make a prepayment using the online service on our company's website, or in cash or using a Bank card from any Bank in our office.",
+              'q': 'What will happen to the Deposit when the lease expires?',
+              "a": 'The Deposit amount will be returned to you without fail within 30 calendar days after the end of the rental period, subject to all the terms of the Contract.'
             },
             {
-              q:"Can another driver be allowed to drive the car?",
-              a: "other drivers may be allowed to drive if they meet the requirements of the Renter, as well as if they have a similar set of documents.",
+              'q': 'Do I have to make a prepayment?',
+              "a": 'A prepayment of 20% of the rental price is a prerequisite for booking a car. After making an advance payment, a specific car will be booked for You, whose personal data (color, configuration, year of manufacture, registration plate) will be indicated in the rental Agreement and the car receipt Voucher.'
             },
             {
-              q:"What should I do if the car crashes through no fault of mine?",
-              a:"In the event of a breakdown, we will promptly and free of charge replace the car with a similar one.",
+              'q': 'How do I make a prepayment?',
+              "a": 'You can make an advance payment using the online service on our company`s website.'
             },
             {
-              q:" what is the procedure for getting a car at Sochi Airport?",
-              a: "Upon arrival at the Sochi Airport, you need to go to the reception of our company, which is located in the arrival terminal.",
+              'q': 'Can I pay the rent by card?',
+              "a": 'You can pay for your car rental with a Master Card, Visa or MIR card at our company`s office.'
             },
             {
-              q: "is it Necessary to leave the car in secured Parking lots during the day?",
-              a:"during the day, we recommend that you do not leave your car unattended unless absolutely necessary. And in the dark, it is desirable to store the car in secure Parking lots.",
+              'q': 'How to find You at the airport of Sochi?',
+              "a": 'Upon arrival at the airport of Sochi, our Manager will contact You. Transfer of the car is carried out in the Parking lot of the Sochi Airport.'
             },
             {
-              q:"What should I do if I have an accident?",
-              a:" In case of an accident, you need to contact our specialists at +7 (988) 282-97-97, then call the traffic police to the scene and get the following documents: a copy of the Protocol, the scheme of the accident and a certificate of the accident.",
+              'q': 'Can another driver be allowed to drive the car?',
+              "a": 'Other drivers can be allowed to drive a car if they meet the requirements for the tenant, as well as if they have a similar set of documents.'
             },
             {
-              q: "are the Cars insured?",
-              a: "All cars of our company are insured under CASCO and CTP. According to the lease Agreement, Your liability is limited to the deductible under the CASCO policy rubles. To remove restrictions, you can use the service \"Cancellation of Deposit and deductible\"."
+              'q': 'Is it necessary to wash and refuel the car before returning it?',
+              "a": 'You will get a clean car with a full tank of gasoline. At the end of the rental period, it is necessary to return it in its pure form, or pay for the car wash in the amount of 1000 rubles. The fuel tank must also be full, or you will need to pay for the missing amount of fuel according to the company`s price list.'
             },
             {
-              q: "When will the Contract and other necessary documents be ready?",
-              a: "Your rental Agreement and other related documents will be ready when you receive the car."
+              'q': 'What fuel should I use to refuel the car?',
+              "a": 'Almost all our cars are refueled with AI-95 gasoline, but there are exceptions, they should be refueled with diesel fuel DT. The information is posted on the fuel tank hatch, be careful!'
             },
             {
-              q: "is it Necessary to wash and refuel the car before returning?",
-              a: "you will get a clean Car with a full tank of gasoline. At the end of the rental period, you must return it in its pure form, or pay for the car wash in the amount of 1000 rubles. The fuel tank must also be full, or you will need to pay for the missing amount of fuel at the rate of 60 rubles per liter."
+              'q': 'The "franchise Cancellation" service - what is it?',
+              "a": 'The service "cancellation of the franchise" cancels the franchise under the CASCO policy for an additional fee, and at the same time obliges to provide all certificates of damage to the vehicle from the traffic police.'
             },
             {
-              q: "What fuel should I use for refueling the car?",
-              a: "to refuel the car, you must use AI-95 gasoline."
+              'q': 'What should I do if the car crashes through no fault of mine?',
+              "a": 'In the event of a breakdown, we will promptly and free of charge replace the car with a similar one, or eliminate the cause of the breakdown in a short time.'
             },
             {
-              q: "We are planning a trip with young children, can you rent child seats?",
-              a: "Yes, of course, you only need to order this service in advance."
+              'q': 'What should I do if I have an accident?',
+              "a": 'In the event of an accident, you need to contact our specialists at <a href="tel:88004444848">8 (800) 444-48-48</a>, then call traffic police officers to the scene and get the following documents: a copy of the Protocol, an accident diagram and a certificate of the accident.'
+            },
+            {
+              'q': 'Do I need to pay an Airport fee?',
+              "a": 'In our company there are no all kinds of fees and additional payments, * Airport fee, surcharge for winter tires and a set of motorists.*'
+            },
+            {
+              'q': 'We are planning a trip with small children, Can you rent child seats?',
+              "a": 'Child seats and boosters can be rented for an additional fee according to the company`s price list. You must book this service in advance.'
+            },
+            {
+              'q': 'What are the daily mileage limits?',
+              "a": 'There is no mileage limit in our company.'
             }
           ]
         }
